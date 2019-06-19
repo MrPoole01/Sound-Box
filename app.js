7074b7162a35cd0230e2c8f41f4ccab6008fe2f4
Recardo Poole <recardo.poole@gmail.com>
2019-06-19 03:28:41 -0400

    functional and is working properly

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-getSounds();
+const soundsElement = document.querySelector('#sounds');
+
+(async () => {
+  const sounds = await getSounds();
+  addSoundsToPage(sounds);
+})();
 
 async function getSounds() {
   const response = await fetch('./sounds.json');
   const json = await response.json();
-  console.log(json);
+  //console.log(json);
+  return json;
+}
+
+function addSoundsToPage(sounds) {
+  sounds.forEach(sound => {
+    const soundDiv = document.createElement('div');
+    soundDiv.className = 'sound';
+
+    const soundTitle = document.createElement('h2');
+    soundTitle.textContent = sound.title;
+    soundDiv.appendChild(soundTitle);
+
+    const playButton = document.createElement('button');
+    playButton.className = 'button';
+    playButton.textContent = '🔊';
+    soundDiv.appendChild(playButton);
+
+    const player = document.createElement('audio');
+    player.setAttribute('src', `audio/${sound.src}`);
+    soundDiv.appendChild(player);
+
+    playButton.addEventListener('click', () => {
+      player.currentTime = 0;
+      player.play();
+    });
+
+    soundsElement.appendChild(soundDiv);
+  });
 }
